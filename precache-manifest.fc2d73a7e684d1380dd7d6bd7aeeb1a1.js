27abb67920d2370a9fdb8b69d0dde4ddd012c62a
Ahmed Hossam <ahmed_7oskaa@pop-os.localdomain>
2022-09-01 14:15:07 +0200

    Updates

---
+++ precache-manifest.fc2d73a7e684d1380dd7d6bd7aeeb1a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b0c786ecc9fe11a58e2",
+    "url": "/static/css/main.b088e125.chunk.css"
+  },
+  {
+    "revision": "7b0c786ecc9fe11a58e2",
+    "url": "/static/js/main.7b0c786e.chunk.js"
+  },
+  {
+    "revision": "c67df3666413ff18363b",
+    "url": "/static/js/1.c67df366.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7bd1b32c3a02c97087b51d091fee29c3",
+    "url": "/index.html"
+  }
+];
