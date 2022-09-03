164099f857adc08d1e0e1f4624b91459ad8f2df6
Ahmed Hossam <ahmed_7oskaa@pop-os.localdomain>
2022-09-03 20:00:08 +0200

    Updates

---
+++ precache-manifest.8ffbb0e78118fbaa012c24860bcc43a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bb8efd375c15651cf04",
+    "url": "/udemy-home-page-React/static/css/main.a9ec5b93.chunk.css"
+  },
+  {
+    "revision": "9bb8efd375c15651cf04",
+    "url": "/udemy-home-page-React/static/js/main.9bb8efd3.chunk.js"
+  },
+  {
+    "revision": "0a16bd601c50fe48988e",
+    "url": "/udemy-home-page-React/static/js/1.0a16bd60.chunk.js"
+  },
+  {
+    "revision": "44c853c174cdefbc569d",
+    "url": "/udemy-home-page-React/static/js/runtime~main.44c853c1.js"
+  },
+  {
+    "revision": "538da60acb38ef9f5f913fc630a89a34",
+    "url": "/udemy-home-page-React/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/udemy-home-page-React/precache-manifest.ca9483597fbe776eab1fe1a6d3df1a2e.js"
+  "/udemy-home-page-React/precache-manifest.8ffbb0e78118fbaa012c24860bcc43a5.js"
 );
 
 workbox.clientsClaim();
