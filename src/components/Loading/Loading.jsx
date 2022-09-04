284827511ff428414973c74121e9a738efa73d40
Ahmed Hossam <ahmed_7oskaa@pop-os.localdomain>
2022-09-04 22:51:43 +0200

    add spinner to page and fetch data from api and make search bar work

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './styles.css'
+import MoonLoader from 'react-spinners/MoonLoader'
+
+function Loading() {
+    return (
+        <MoonLoader className='loading'/>
+    )
+}
+
+export default Loading
